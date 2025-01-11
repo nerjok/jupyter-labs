133c3ccb526ec1028048c015092ddf9d72b0c4e1
Nerijus Jokubauskas <neriejus@gmail.com>
2025-01-11 13:05:34 +0200

    multiclass variation and numer recognition

---
+++ utils/autils.py
@@ -0,0 +1,38 @@
+import numpy as np
+
+def load_data():
+    X = np.load("data/X.npy")
+    y = np.load("data/y.npy")
+    X = X[0:1000]
+    y = y[0:1000]
+    return X, y
+
+def load_weights():
+    w1 = np.load("data/w1.npy")
+    b1 = np.load("data/b1.npy")
+    w2 = np.load("data/w2.npy")
+    b2 = np.load("data/b2.npy")
+    return w1, b1, w2, b2
+
+def sigmoid(x):
+    return 1. / (1. + np.exp(-x))
+
+def plt_act_trio():
+    X = np.linspace(-5,5,100)
+    fig,ax = plt.subplots(1,3, figsize=(6,2))
+    widgvis(fig)
+    ax[0].plot(X,tf.keras.activations.linear(X))
+    ax[0].axvline(0, lw=0.3, c="black")
+    ax[0].axhline(0, lw=0.3, c="black")
+    ax[0].set_title("Linear")
+    ax[1].plot(X,tf.keras.activations.sigmoid(X))
+    ax[1].axvline(0, lw=0.3, c="black")
+    ax[1].axhline(0, lw=0.3, c="black")
+    ax[1].set_title("Sigmoid")
+    ax[2].plot(X,tf.keras.activations.relu(X))
+    ax[2].axhline(0, lw=0.3, c="black")
+    ax[2].axvline(0, lw=0.3, c="black")
+    ax[2].set_title("ReLu")
+    fig.suptitle("Common Activation Functions", fontsize=14)
+    fig.tight_layout(pad=0.2)
+    plt.show()
